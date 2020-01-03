4be23e755a6c7de1b4d50b9240048173d1b9ff67
wangning <1234qwer>
2020-01-03 10:20:03 +0800

    修改各个模块的yml配置文件

--- hope-server-service/src/main/java/com/server/service/ServiceApplication.java
+++ hope-server-service/src/main/java/com/server/service/ServiceApplication.java
@@ -2,12 +2,17 @@ package com.server.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author wangning
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
 
 }
