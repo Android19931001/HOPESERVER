14c3b041b1437e1f8f5a54144fe9cf757acf0f41
wangning <1234qwer>
2022-12-30 13:43:49 +0800

    feignClient增加url属性，修改端口

--- hope-server-controller/src/main/java/com/hope/server/business/versionRelease/HopeServiceClient.java
+++ hope-server-controller/src/main/java/com/hope/server/business/versionRelease/HopeServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * @author wangning
  */
-@FeignClient(value = "hope-server-service",url = "http://localhost:1041")
+@FeignClient(value = "hope-server-service",url = "http://localhost:4671")
 public interface HopeServiceClient {
 
     /**
